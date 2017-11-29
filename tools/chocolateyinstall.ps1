b97d602a94333b21144eb99d8d6eefd4fe78c025
dschelchkov <dschelchkov@walmartlabs.com>
2017-11-29 11:08:09 -0800

    first commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName= 'PSWindowsUpdate' # arbitrary name for the package, used in messages
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$dataDir = Join-Path (Split-Path $toolsDir) 'data'
+
+$targetDir = "$Env:WinDir\System32\WindowsPowerShell\v1.0\Modules\$packageName"
+
+if (Test-Path -PathType Container $targetDir)
+{ Remove-Item "$targetDir\*"}
+else
+{ New-Item -ItemType Directory $targetDir}
+
+Copy-Item "$dataDir\*" $targetDir
